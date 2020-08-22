9433b997b089871fe9b0f2dcd1c2196ca1580fae
willbe03 <aotuman233@gmail.com>
2020-08-23 00:54:42 +0800

    d

--- src/main/kotlin/pigeon/qqbot/LoginTest.kt
+++ src/main/kotlin/pigeon/qqbot/LoginTest.kt
@@ -8,19 +8,26 @@ import net.mamoe.mirai.message.data.At
 import net.mamoe.mirai.contact.Member
 import net.mamoe.mirai.event.subscribeMessages
 
+
+
 suspend fun main() {
-    val qqId = 10000L//Bot的QQ号，需为Long类型，在结尾处添加大写L
-    val password = "your_password"//Bot的密码
+    val qqId = 12412L//Bot的QQ号，需为Long类型，在结尾处添加大写L
+    val password = "123124"//Bot的密码
     val miraiBot = Bot(qqId, password).alsoLogin()//新建Bot并登录
     miraiBot.subscribeMessages {
         "你好" reply "你好!"
+        //"#查询二次元浓度" reply "${(Math.random()*100)}%"
         case("at me") {
             reply(At(sender as Member) + " 给爷爬 ")
         }
 
-        (contains("舔") or contains("刘老板")) {
-            reply("刘老板太强了")
+        (contains("舔") or contains("veraku",true)) {
+            reply("veraku是神")
         }
+        (contains("nmsl")) {
+            reply("nmysl")
+        }
+
     }
     miraiBot.join() // 等待 Bot 离线, 避免主线程退出
 }
