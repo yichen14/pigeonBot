b4a9d2e719de610397b562df68eb670bdbd452ed
willbe03 <aotuman233@gmail.com>
2020-08-23 15:30:49 +0800

    repaired bugs in 查询二次元浓度

--- src/main/kotlin/pigeon/qqbot/LoginTest.kt
+++ src/main/kotlin/pigeon/qqbot/LoginTest.kt
@@ -18,21 +18,24 @@ import kotlin.math.roundToInt
 
 
 suspend fun main() {
-    val qqId = 1L//Bot的QQ号，需为Long类型，在结尾处添加大写L
-    val password = "!"//Bot的密码
+    val qqId = 3308443151L//Bot的QQ号，需为Long类型，在结尾处添加大写L
+    val password = "Qwerasdf!"//Bot的密码
     val miraiBot = Bot(qqId, password).alsoLogin()//新建Bot并登录
     miraiBot.subscribeMessages {
-        "你好" reply "你好!"
-        "#查询二次元浓度" reply "${(Math.random()*100)}%" as String
         case("at me") {
             reply(At(sender as Member) + " 给爷爬 ")
         }
-
+        case("#查询二次元浓度"){
+            reply("${(0..100).random() as Int}%")
+        }
         (contains("舔") or contains("veraku",true)) {
             reply("veraku是神")
         }
         (contains("nmsl")) {
             reply("nmysl")
+        }
+        contains("不行"){
+            reply("不许不行")
         }
         case("芳芳"){
             reply("芳芳是神")
@@ -40,9 +43,10 @@ suspend fun main() {
         case("神话语录"){
             randomImg("mythquotes").sendAsImageTo(subject)
         }
-        case("bfm"){
+        contains("fm"){
             randomImg("cats").sendAsImageTo(subject)
         }
+
     }
     miraiBot.join() // 等待 Bot 离线, 避免主线程退出
 }
