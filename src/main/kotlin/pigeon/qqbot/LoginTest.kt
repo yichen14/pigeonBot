5e3131d6ff895cd1059a6503ecf59af6c8af2c84
willbe03 <aotuman233@gmail.com>
2020-08-23 20:04:09 +0800

    put features in functions

--- src/main/kotlin/pigeon/qqbot/LoginTest.kt
+++ src/main/kotlin/pigeon/qqbot/LoginTest.kt
@@ -24,48 +24,60 @@ suspend fun main() {
     val qqId = 3308443151L//Bot的QQ号，需为Long类型，在结尾处添加大写L
     val password = "Qwerasdf!"//Bot的密码
     val miraiBot = Bot(qqId, password).alsoLogin()//新建Bot并登录
-    miraiBot.subscribeMessages {
-        case("at me") {
-            reply(At(sender as Member) + " 给爷爬 ")
-        }
-        case("#查询二次元浓度"){
-            reply("${(0..100).random()}%")
-        }
-        (contains("舔") or contains("veraku",true)) {
-            reply("veraku是神")
-        }
-        contains("nmsl") {
-            reply("nmysl")
-        }
-        contains("不行"){
-            reply("不许不行")
-        }
-        case("芳芳"){
-            reply("芳芳是神")
-        }
-        case("神话语录"){
-            randomImg("mythquotes")?.sendAsImageTo(subject)
-        }
-        case("bfm"){
-            randomImg("cats")?.sendAsImageTo(subject)
-        }
-        (contains("技校") or contains("废物")) {
-            reply("虚伪b快爬")
-        }
-
-    }
+    miraiBot.keywordReply()
+    miraiBot.randomRepeat()
     miraiBot.subscribeAlways<NewFriendRequestEvent> { event->
         if(event.message.contains("鸽舍")){
             event.accept()
             miraiBot.getFriend(event.fromId).sendMessage("nmsl")
         }
-    }
+    }/*
     miraiBot.subscribeAlways<GroupMessageEvent> {
         if((1..3).random()==1) {
             reply(message)//33.3%概率复读
         }
-    }
+    }*/
     miraiBot.join() // 等待 Bot 离线, 避免主线程退出
 }
 
 fun randomImg(path:String) = File("src/img/$path").listFiles()?.random()
+
+fun Bot.keywordReply(){
+    this.subscribeMessages {
+        case("at me") {
+            reply(At(sender as Member) + " 给爷爬 ")
+        }
+        case("#查询二次元浓度") {
+            reply("${(0..100).random()}%")
+        }
+        (contains("舔") or contains("veraku", true)) {
+            reply("veraku是神")
+        }
+        contains("nmsl") {
+            reply("nmysl")
+        }
+        contains("不行") {
+            reply("不许不行")
+        }
+        case("芳芳") {
+            reply("芳芳是神")
+        }
+        case("神话语录") {
+            randomImg("mythquotes")?.sendAsImageTo(subject)
+        }
+        case("bfm") {
+            randomImg("cats")?.sendAsImageTo(subject)
+        }
+        (contains("技校") or contains("废物")) {
+            reply("虚伪b快爬")
+        }
+    }
+}
+
+fun Bot.randomRepeat(){
+    this.subscribeAlways<GroupMessageEvent> {
+        if((1..50).random()==1) {
+            reply(message)//2%概率复读
+        }
+    }
+}
