1a4ff47372f463baeda7fdd29bff0af59a452cb0
veraku <a146665154@163.com>
2020-08-28 21:33:26 +0800

    色图功能优化

--- src/main/kotlin/pigeon/qqbot/Setu.kt
+++ src/main/kotlin/pigeon/qqbot/Setu.kt
@@ -2,29 +2,43 @@ package pigeon.qqbot
 
 import com.beust.klaxon.Klaxon
 import net.mamoe.mirai.Bot
+import net.mamoe.mirai.contact.Member
 import net.mamoe.mirai.event.subscribeMessages
+import net.mamoe.mirai.message.data.At
+import net.mamoe.mirai.message.data.PlainText
+import net.mamoe.mirai.message.data.messageChainOf
 import net.mamoe.mirai.message.sendAsImageTo
 import java.io.File
 import java.net.HttpURLConnection
 import java.net.URL
 import java.net.URLEncoder
 
-data class Img(val url:String)
-data class Rpl(val data: List<Img>)
+data class Img(val url: String)
+data class Rpl(val code: Int, val data: List<Img>)
 
-private fun getSetu(xp: String): String {
-    val http=
-            URL("https://api.lolicon.app/setu/?apikey=432105395f48f8888acb81&keyword=${URLEncoder.encode(xp,"UTF-8")}")
-                    .openConnection() as HttpURLConnection
-    http.requestMethod="GET"
-    val json= Klaxon().parse<Rpl>(http.inputStream)
-    return saveImg(json?.data?.random()?.url,"setu")
-}
+private val lastTime = mutableMapOf<Long, Long>()
 
 fun Bot.setu() {
     this.subscribeMessages {
         startsWith("#色图", true) {
-            File("src/img/setu/${getSetu(it)}.jpg").sendAsImageTo(subject)
+            if (lastTime.containsKey(this.sender.id) && System.currentTimeMillis() - lastTime[this.sender.id]!! <= 1000 * 60 * 10) {
+                reply(messageChainOf(PlainText("冲太多了不好哦"), At(this.sender as Member)))
+            } else {
+                lastTime[this.sender.id] = System.currentTimeMillis()
+                val http =
+                        URL("https://api.lolicon.app/setu/?apikey=432105395f48f8888acb81&keyword=${URLEncoder.encode(it, "UTF-8")}")
+                                .openConnection() as HttpURLConnection
+                http.requestMethod = "GET"
+                val json = Klaxon().parse<Rpl>(http.inputStream)
+                when (json?.code) {
+                    0 -> {
+                        val md5 = saveImg(json.data.random().url, "setu")
+                        File("/src/img/setu/$md5.jpg").sendAsImageTo(subject)
+                    }
+                    404 -> reply("找不到关键词为${it}的色图")
+                    429 -> reply("今日色图配额已用尽，你们这能冲啊")
+                }
+            }
         }
     }
 }
