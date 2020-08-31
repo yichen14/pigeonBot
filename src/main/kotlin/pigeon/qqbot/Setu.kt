d05d7dad4f5d9cb5845d5b2de4022f21c6507ef4
veraku <a146665154@163.com>
2020-08-31 10:35:13 +0800

    添加danbooru搜索

--- src/main/kotlin/pigeon/qqbot/Setu.kt
+++ src/main/kotlin/pigeon/qqbot/Setu.kt
@@ -1,5 +1,6 @@
 package pigeon.qqbot
 
+import com.beust.klaxon.Json
 import com.beust.klaxon.Klaxon
 import net.mamoe.mirai.Bot
 import net.mamoe.mirai.contact.Member
@@ -13,8 +14,10 @@ import java.net.HttpURLConnection
 import java.net.URL
 import java.net.URLEncoder
 
-data class Img(val url: String)
-data class Rpl(val code: Int, val data: List<Img>)
+data class LoliconImg(val url: String)
+data class LoliconRpl(val code: Int, val data: List<LoliconImg>)
+
+data class DanbooruImg(@Json(name = "file_url") val fileURL: String)
 
 private val lastTime = mutableMapOf<Long, Long>()
 
@@ -23,20 +26,34 @@ fun Bot.setu() {
         startsWith("#色图", true) {
             if (lastTime.containsKey(this.sender.id) && System.currentTimeMillis() - lastTime[this.sender.id]!! <= 1000 * 60 * 10) {
                 reply(messageChainOf(PlainText("冲太多了不好哦"), At(this.sender as Member)))
-            } else {
+            } else if (it.contains("danbooru")) {
+                val xp = URLEncoder.encode(it.replace("danbooru", "").trim().replace(" ", "_"), "UTF-8")
                 val http =
-                        URL("https://api.lolicon.app/setu/?apikey=432105395f48f8888acb81&keyword=${URLEncoder.encode(it, "UTF-8")}")
+                        URL("https://danbooru.donmai.us/posts.json?tags=$xp")
                                 .openConnection() as HttpURLConnection
                 http.requestMethod = "GET"
-                val json = Klaxon().parse<Rpl>(http.inputStream)
+                val json = Klaxon().parseArray<DanbooruImg>(http.inputStream)
+                if (json?.isEmpty()!!)
+                    reply("找不到关键词为${xp}的色图")
+                else {
+                    val md5 = saveImg(json.random().fileURL, "setu")
+                    File("src/img/setu/$md5.jpg").sendAsImageTo(subject)
+                }
+            } else {
+                val xp = URLEncoder.encode(it, "UTF-8")
+                val http =
+                        URL("https://api.lolicon.app/setu/?apikey=432105395f48f8888acb81&keyword=$xp")
+                                .openConnection() as HttpURLConnection
+                http.requestMethod = "GET"
+                val json = Klaxon().parse<LoliconRpl>(http.inputStream)
                 when (json?.code) {
                     0 -> {
                         val md5 = saveImg(json.data.random().url, "setu")
                         File("src/img/setu/$md5.jpg").sendAsImageTo(subject)
                         lastTime[this.sender.id] = System.currentTimeMillis()
                     }
-                    404 -> reply("找不到关键词为${it}的色图")
-                    429 -> reply("今日色图配额已用尽，你们这能冲啊")
+                    404 -> reply("找不到关键词为${xp}的色图")
+                    429 -> reply("今日色图配额已用尽，你们真能冲啊")
                 }
             }
         }
