396e262967c363afce2e095d62c9280b52cd87a6
veraku <a146665154@163.com>
2020-09-06 00:06:33 +0800

    pixivic功能

--- src/main/kotlin/pigeon/qqbot/Setu.kt
+++ src/main/kotlin/pigeon/qqbot/Setu.kt
@@ -1,6 +1,5 @@
 package pigeon.qqbot
 
-import com.beust.klaxon.Json
 import com.beust.klaxon.Klaxon
 import net.mamoe.mirai.Bot
 import net.mamoe.mirai.contact.Member
@@ -17,29 +16,20 @@ import java.net.URLEncoder
 data class LoliconImg(val url: String)
 data class LoliconRpl(val code: Int, val data: List<LoliconImg>)
 
-data class DanbooruImg(@Json(name = "file_url") val fileURL: String)
+data class PixivIcUrl(val original: String)
+data class PixivIcIllust(val imageUrls: List<PixivIcUrl>)
+data class PixivIcRpl(val data: List<PixivIcIllust>)
 
 private val lastTime = mutableMapOf<Long, Long>()
 
 fun Bot.setu() {
     this.subscribeMessages {
-        startsWith("#色图", true) {
+
+            startsWith("#色图", true) {
             if (lastTime.containsKey(this.sender.id) && System.currentTimeMillis() - lastTime[this.sender.id]!! <= 1000 * 60 * 10) {
                 reply(messageChainOf(PlainText("冲太多了不好哦"), At(this.sender as Member)))
-            } else if (it.contains("danbooru")) {
-                val xp = URLEncoder.encode(it.replace("danbooru", "").trim().replace(" ", "_"), "UTF-8")
-                val http = URL("https://danbooru.donmai.us/posts.json?tags=$xp")
-                        .openConnection() as HttpURLConnection
-                http.requestMethod = "GET"
-                val json = Klaxon().parseArray<DanbooruImg>(http.inputStream)
-                if (json?.isEmpty()!!)
-                    reply("找不到关键词为${xp}的色图")
-                else {
-                    val md5 = saveImg(json.random().fileURL, "setu")
-                    File("src/img/setu/$md5.jpg").sendAsImageTo(subject)
-                }
             } else {
-                val xp = URLEncoder.encode(it, "UTF-8")
+                var xp = URLEncoder.encode(it, "UTF-8")
                 val http =
                         URL("https://api.lolicon.app/setu/?apikey=432105395f48f8888acb81&keyword=$xp")
                                 .openConnection() as HttpURLConnection
@@ -51,7 +41,21 @@ fun Bot.setu() {
                         File("src/img/setu/$md5.jpg").sendAsImageTo(subject)
                         lastTime[this.sender.id] = System.currentTimeMillis()
                     }
-                    404 -> reply("找不到关键词为${it}的色图")
+                    404 -> {
+                        try {
+                            xp=xp.replace("pixivic","").trim()
+                            val http =
+                                URL("https://api.pixivic.com/illustrations?keyword=$xp&page=1&searchType=autoTranslate&illustType=illust")
+                                    .openConnection() as HttpURLConnection
+                            http.requestMethod = "GET"
+                            val json = Klaxon().parse<PixivIcRpl>(http.inputStream)
+                            val url=json?.data?.random()?.imageUrls?.random()?.original
+                            val md5 = saveImg(url?.replace("pximg.net","pixiv.cat"), "setu")
+                            File("src/img/setu/$md5.jpg").sendAsImageTo(subject)
+                        } catch(e: Exception) {
+                        reply("找不到关键词为${it}的色图")
+                        }
+                    }
                     429 -> reply("今日色图配额已用尽，你们真能冲啊")
                 }
             }
