5f3214a356a2f6d58ceb02dc278a5555f9d17101
willbe <aotuman233@gmail.com>
2020-09-16 21:46:14 +0700

    fixed a bug in Quote.kt

--- src/main/kotlin/pigeon/qqbot/Quote.kt
+++ src/main/kotlin/pigeon/qqbot/Quote.kt
@@ -4,19 +4,23 @@ import net.mamoe.mirai.Bot
 import net.mamoe.mirai.event.subscribeMessages
 import net.mamoe.mirai.message.data.At
 import net.mamoe.mirai.message.data.Image
+import net.mamoe.mirai.message.data.content
 import net.mamoe.mirai.message.data.queryUrl
 import net.mamoe.mirai.message.sendAsImageTo
 
 fun Bot.quote() {
     this.subscribeMessages {
         startsWith("#上传语录") {
-            val img = message[Image]
-            val at = message[At]
-            saveImg(img?.queryUrl(), "${at?.target}quotes")
-            reply("添加成功")
+            if(message[At]!=null) {
+                val img = message[Image]
+                val at = message[At]
+                saveImg(img?.queryUrl(), "${at?.target}quotes")
+                reply("添加成功")
+            }
         }
         startsWith("#语录") {
-            randomImg("${message[At]?.target}quotes")?.sendAsImageTo(subject)
+            if(message[At]!=null)
+                randomImg("${message[At]?.target}quotes")?.sendAsImageTo(subject)
         }
     }
 }
