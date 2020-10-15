29bbb89880eabc9f539967c9174f4dd91be159b2
veraku <a146665154@163.com>
2020-10-15 23:03:13 +0800

    把代码改成符合我xp的样子

--- src/main/kotlin/pigeon/qqbot/Setu.kt
+++ src/main/kotlin/pigeon/qqbot/Setu.kt
@@ -24,35 +24,34 @@ private val lastTime = mutableMapOf<Long, Long>()
 
 fun Bot.setu() {
     this.subscribeMessages {
-            startsWith("#色图", true) {
+        startsWith("#色图", true) {
             if (lastTime.containsKey(this.sender.id) && System.currentTimeMillis() - lastTime[this.sender.id]!! <= 1000 * 60 * 10) {
                 reply(messageChainOf(PlainText("冲太多了不好哦"), At(this.sender as Member)))
             } else {
                 var xp = URLEncoder.encode(it, "UTF-8")
-                val http =
-                        URL("https://api.lolicon.app/setu/?apikey=432105395f48f8888acb81&keyword=$xp")
-                                .openConnection() as HttpURLConnection
-                http.requestMethod = "GET"
-                val json = Klaxon().parse<LoliconRpl>(http.inputStream)
+                val json =
+                    getJson<LoliconRpl>("https://api.lolicon.app/setu/?apikey=432105395f48f8888acb81&keyword=$xp")
                 when (json?.code) {
                     0 -> {
-                        val md5 = saveImg(json.data.random().url, "setu")
-                        File("src/img/setu/$md5.jpg").sendAsImageTo(subject)
-                        lastTime[this.sender.id] = System.currentTimeMillis()
+                        try {
+                            val md5 = saveImg(json.data.random().url, "setu")
+                            File("src/img/setu/$md5.jpg").sendAsImageTo(subject)
+                            lastTime[this.sender.id] = System.currentTimeMillis()
+                        } catch (e: Exception) {
+                            reply("发送色图时发生错误")
+                        }
                     }
                     404 -> {
+                        xp = xp.replace("pixivic", "").trim()
                         try {
-                            xp=xp.replace("pixivic","").trim()
-                            val http =
-                                URL("https://api.pixivic.com/illustrations?keyword=$xp&page=1&illustType=illust")
-                                    .openConnection() as HttpURLConnection
-                            http.requestMethod = "GET"
-                            val json = Klaxon().parse<PixivIcRpl>(http.inputStream)
-                            val url= json?.data?.get((0..5).random())?.imageUrls?.random()?.original
-                            val md5 = saveImg(url?.replace("pximg.net","pixiv.cat"), "setu")
+                            val url =
+                                getJson<PixivIcRpl>("https://api.pixivic.com/illustrations?keyword=$xp&page=1&illustType=illust")?.data?.get(
+                                    (0..5).random()
+                                )?.imageUrls?.random()?.original
+                            val md5 = saveImg(url?.replace("pximg.net", "pixiv.cat"), "setu")
                             File("src/img/setu/$md5.jpg").sendAsImageTo(subject)
-                        } catch(e: Exception) {
-                        reply("找不到关键词为${it}的色图")
+                        } catch (e: Exception) {
+                            reply("找不到关键词为${xp}的色图")
                         }
                     }
                     429 -> reply("今日色图配额已用尽，你们真能冲啊")
@@ -60,4 +59,10 @@ fun Bot.setu() {
             }
         }
     }
+}
+
+private inline fun <reified json> getJson(url: String): json? {
+    val http = URL(url).openConnection() as HttpURLConnection
+    http.requestMethod = "GET"
+    return Klaxon().parse<json>(http.inputStream)
 }
