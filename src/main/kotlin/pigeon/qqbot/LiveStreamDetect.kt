e2acb6d78da62d49b4ff62fa532f68703b2a978e
veraku <a146665154@163.com>
2020-10-15 22:45:22 +0800

    直播提醒功能修缮

--- src/main/kotlin/pigeon/qqbot/LiveStreamDetect.kt
+++ src/main/kotlin/pigeon/qqbot/LiveStreamDetect.kt
@@ -4,32 +4,29 @@ import com.beust.klaxon.Klaxon
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import net.mamoe.mirai.Bot
 import java.net.HttpURLConnection
 import java.net.URL
-import net.mamoe.mirai.Bot
+
 data class Douyu(val error: Int, val data: data)
-data class data(val room_name:String, val room_status: String)
+data class data(val room_name: String, val room_status: String)
 
 
-fun Bot.LiveStreamDetect(roomNumber:Int) = runBlocking<Unit> {
-    val job = launch {
-        var liveStatus: String = "0"
+fun Bot.liveStreamDetect(roomNumber: Int) {
+    GlobalScope.launch {
+        var liveStatus = "0"
         while (true) {
             val http =
-                    URL("http://open.douyucdn.cn/api/RoomApi/room/$roomNumber")
-                            .openConnection() as HttpURLConnection
+                URL("http://open.douyucdn.cn/api/RoomApi/room/$roomNumber")
+                    .openConnection() as HttpURLConnection
             http.requestMethod = "GET"
             val json = Klaxon().parse<Douyu>(http.inputStream)
             if (json?.error == 0) {
-                if (json.data.room_status == "1" && json.data.room_status != liveStatus) {
-                    liveStatus = json.data.room_status
-                    getGroup(596870824L).sendMessage("$roomNumber 播了\n${json.data.room_name}")
-                } else {
-                    liveStatus = json.data.room_status
-                }
+                if (json.data.room_status == "1" && json.data.room_status != liveStatus)
+                    getGroup(596870824L).sendMessage("$roomNumber 播了")
+                liveStatus = json.data.room_status
             }
-            delay(100000L)
+            delay(1000L)
         }
     }
 }
