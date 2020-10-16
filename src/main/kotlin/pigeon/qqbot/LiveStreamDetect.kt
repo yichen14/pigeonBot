f6b96d565474fc20144744431383bf37de7d2c82
veraku <a146665154@163.com>
2020-10-16 19:38:08 +0800

    直播提醒功能修缮

--- src/main/kotlin/pigeon/qqbot/LiveStreamDetect.kt
+++ src/main/kotlin/pigeon/qqbot/LiveStreamDetect.kt
@@ -1,15 +1,12 @@
 package pigeon.qqbot
 
-import com.beust.klaxon.Klaxon
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import net.mamoe.mirai.Bot
-import java.net.HttpURLConnection
-import java.net.URL
 
-data class Douyu(val error: Int, val data: data)
-data class data(val room_name: String, val room_status: String)
+data class Douyu(val error: Int, val data: Data)
+data class Data(val room_name: String, val room_status: String)
 
 
 fun Bot.liveStreamDetect(roomNumber: Int) {
@@ -19,7 +16,7 @@ fun Bot.liveStreamDetect(roomNumber: Int) {
             val json = getJson<Douyu>("http://open.douyucdn.cn/api/RoomApi/room/$roomNumber")
             if (json?.error == 0) {
                 if (json.data.room_status == "1" && json.data.room_status != liveStatus)
-                    getGroup(1143577518L).sendMessage("$roomNumber 播了")
+                    getGroup(596870824L).sendMessage("$roomNumber 播了")
                 liveStatus = json.data.room_status
             }
             delay(1000L)
