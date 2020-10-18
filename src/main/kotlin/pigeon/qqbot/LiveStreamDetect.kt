25da99148efc622dab75eaddeca349386fd23a33
willbe <aotuman233@gmail.com>
2020-10-18 20:42:48 +0800

    new feature: add start time in livestreamdetect

--- src/main/kotlin/pigeon/qqbot/LiveStreamDetect.kt
+++ src/main/kotlin/pigeon/qqbot/LiveStreamDetect.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.launch
 import net.mamoe.mirai.Bot
 
 data class Douyu(val error: Int, val data: Data)
-data class Data(val room_name: String, val room_status: String)
+data class Data(val room_name: String, val room_status: String, val start_time: String)
 
 
 fun Bot.liveStreamDetect(roomNumber: Int) {
@@ -16,7 +16,7 @@ fun Bot.liveStreamDetect(roomNumber: Int) {
             val json = getJson<Douyu>("http://open.douyucdn.cn/api/RoomApi/room/$roomNumber")
             if (json?.error == 0) {
                 if (json.data.room_status == "1" && json.data.room_status != liveStatus)
-                    getGroup(596870824L).sendMessage("wjqbl\n${json.data.room_name}")
+                    getGroup(596870824L).sendMessage("wjqbl\n${json.data.room_name}\n${json.data.start_time}")
                 liveStatus = json.data.room_status
             }
             delay(100000L)//100s
