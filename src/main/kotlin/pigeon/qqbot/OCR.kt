e6aeaf1279678a8ccf080f15a7b8867fb842e1c7
斯哈 <shigure.siha@Outlook.com>
2020-10-27 22:32:41 +0800

    瞎几把完善OCR

--- src/main/kotlin/pigeon/qqbot/OCR.kt
+++ src/main/kotlin/pigeon/qqbot/OCR.kt
@@ -1,21 +1,50 @@
 package pigeon.qqbot
+
 import com.beust.klaxon.JsonValue
+import net.mamoe.mirai.Bot
+import net.mamoe.mirai.event.subscribeMessages
+import net.mamoe.mirai.message.data.Image
+import java.io.File
 import okhttp3.*
-import java.io.IOException
 
 class OCR {
     private val apiKey = "140b4b8ee688957"
     private val url = "https://api.ocr.space/parse/image"
-    val request = Request.Builder()
+    private val lang = "chs"
+    val client = new OkHttpClient()
+
+    val request = new Request.Builder()
         .url(url)
         .header("apikey", apiKey)
-        .header("language","chs")
+        .header("language",lang)
         .build()
 
-    val client = OkHttpClient()
+    private fun fetchJson(imageUrl: String?):String{
+        request.add("url",imageUrl)
+        Response response = client.newCall(request).execute();
+        if (response.has("ErrorMessage")){
+            val error = response.getString("ErrorMessage")
+            return error
+        }
+        else{
+            val text = response.getString("ParsedText")
+            return text
+        }
 
-
-    private fun fetchJson(){
-        client.newCall(request)
+    }
+    fun Bot.OCR(){
+        this.subscribeMessages{
+            startsWith("#OCR"){
+                if（message[Image] != null） {
+                    val img = message[Image]
+                    val md5 = saveImg(img?.queryUrl(),"114514")
+                    val text = fetchJson(File("src/img/114514/$md5.jpg"))
+                    reply(text) 
+                }
+                else{
+                    reply("ERROR")
+                }
+            }
+        }   
     }
 }
