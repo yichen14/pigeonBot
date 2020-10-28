77a2b98ea40047914aa08a52a1f6aacb64fe0ac8
斯哈 <shigure.siha@Outlook.com>
2020-10-28 18:42:54 +0800

    测试ocr

--- src/main/kotlin/pigeon/qqbot/Main.kt
+++ src/main/kotlin/pigeon/qqbot/Main.kt
@@ -28,6 +28,6 @@ suspend fun main() {
     miraiBot.misc()
     miraiBot.help()
     miraiBot.smartReply()
-    miraiBot.ocr() //测试中，暂不上线
+    miraiBot.ocr() //测试中
     miraiBot.join() // 等待 Bot 离线, 避免主线程退出
 }
