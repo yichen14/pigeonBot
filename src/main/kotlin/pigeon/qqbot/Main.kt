2d36556124d02f77b5d662e6330713f62d565100
veraku <a146665154@163.com>
2020-11-25 20:01:42 +0800

    色图更新源为PixivPy

--- src/main/kotlin/pigeon/qqbot/Main.kt
+++ src/main/kotlin/pigeon/qqbot/Main.kt
@@ -7,14 +7,19 @@ import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.constructor.Constructor
 import java.io.File
 
-data class Config(var qqID: Long = 0, var password: String = "")
+data class Config(
+    var qqID: Long = 0,
+    var qqPassword: String = "",
+    var pixivUsername: String = "",
+    var pixivPassword: String = ""
+)
 
 const val configPath = "src/main/resources/config.yml"
 
 suspend fun main() {
     val config = Yaml(Constructor(Config::class.java)).load(File(configPath).inputStream()) as Config
     val qqId = config.qqID//Bot的QQ号，需为Long类型，在结尾处添加大写L
-    val password = config.password//Bot的密码
+    val password = config.qqPassword//Bot的密码
     val miraiBot = Bot(qqId, password) { fileBasedDeviceInfo() }.alsoLogin()//新建Bot并登录
     miraiBot.liveStreamDetect(6979222, "wjqbl")
     miraiBot.catImage()
@@ -25,7 +30,7 @@ suspend fun main() {
     miraiBot.roll()
     miraiBot.openGame()
     miraiBot.push()
-    miraiBot.setu()
+    miraiBot.setu(config.pixivUsername, config.pixivPassword)
     miraiBot.misc()
     miraiBot.help()
     miraiBot.smartReply()
--- src/main/kotlin/pigeon/qqbot/Setu.kt
+++ src/main/kotlin/pigeon/qqbot/Setu.kt
@@ -8,9 +8,9 @@ import net.mamoe.mirai.message.data.At
 import net.mamoe.mirai.message.data.PlainText
 import net.mamoe.mirai.message.data.messageChainOf
 import net.mamoe.mirai.message.sendAsImageTo
-import okhttp3.Headers
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import org.python.core.PyFunction
+import org.python.core.PyString
+import org.python.util.PythonInterpreter
 import java.io.File
 import java.net.HttpURLConnection
 import java.net.URL
@@ -19,13 +19,13 @@ import java.net.URLEncoder
 data class LoliconImg(val url: String)
 data class LoliconRpl(val code: Int, val data: List<LoliconImg>)
 
-data class PixivIcUrl(val original: String)
-data class PixivIcIllust(val imageUrls: List<PixivIcUrl>)
-data class PixivIcRpl(val data: List<PixivIcIllust>)
-
 private val lastTime = mutableMapOf<Long, Long>()
 
-fun Bot.setu() {
+fun Bot.setu(username: String, password: String) {
+    val interp = PythonInterpreter()
+    interp.execfile("src/main/setu.py")
+    val api = interp.get("login", PyFunction::class.java).__call__(PyString(username), PyString(password))
+    val search = interp.get("search", PyFunction::class.java)
     this.subscribeMessages {
         startsWith("#色图", true) {
             if (lastTime.containsKey(this.sender.id) && System.currentTimeMillis() - lastTime[this.sender.id]!! <= 1000 * 60 * 3) {
@@ -45,30 +45,13 @@ fun Bot.setu() {
                         }
                     }
                     404 -> {
-                        xp = xp.replace("pixivic", "").trim()
-                        try {/*
-                            val url =
-                                getJson<PixivIcRpl>("https://api.pixivic.com/illustrations?keyword=$xp&page=1&illustType=illust")?.data?.get(
-                                    (0..5).random()
-                                )?.imageUrls?.random()?.original*/
-                            val simpleUrl = "https://api.pixivic.com/illustrations?keyword=$xp&page=1&illustType=illust"
-                            val authorization = "eyJhbGciOiJIUzUxMiJ9.eyJwZXJtaXNzaW9uTGV2ZWwiOjIsInJlZnJlc2hDb3VudCI6MSwiaXNCYW4iOjEsInVzZXJJZCI6NTYwMDA4LCJpYXQiOjE2MDUxOTU1NjYsImV4cCI6MTYwNTM2ODM2Nn0.86cYaJsgv_UHh0U5tfQR9D6wzwv6QXZNZGrPlyc61tjHH0EK4fzmrKk73dBDvTiQ_z4T7j6dFWXEfbtntdzayQ"
-                            val headers = Headers.Builder()
-                                .add("Authorization", authorization)
-                                .build()
-                            val request = Request.Builder()
-                                .url(simpleUrl)
-                                .headers(headers)
-                                .get()
-                                .build()
-                            val client = OkHttpClient()
-                            val response = client.newCall(request).execute().body()!!.string()
-                            println(response)
-                            val result = Klaxon().parse<PixivIcRpl>(response)?.data?.get((0..5).random())?.imageUrls?.random()?.original
-                            val md5 = saveImg(result?.replace("pximg.net", "pixiv.cat"), "setu")
+                        xp = xp.replace("pixiv", "").trim()
+                        try {
+                            val md5 = saveImg(search.__call__(api, PyString(xp)).toString(), "setu")
                             File("src/img/setu/$md5.jpg").sendAsImageTo(subject)
+                            lastTime[this.sender.id] = System.currentTimeMillis()
                         } catch (e: Exception) {
-                            reply("找不到关键词为${it.replace("pixivic","")}的色图")
+                            reply("找不到关键词为${it.replace("pixiv", "")}的色图")
                         }
                     }
                     429 -> reply("今日色图配额已用尽，你们真能冲啊")
