9ab4d2397c317d31e7256efacf1fc0b3f4829de8
veraku <a146665154@163.com>
2020-11-27 00:08:03 +0800

    色图功能再强化

--- src/main/kotlin/pigeon/qqbot/Welcome.kt
+++ src/main/kotlin/pigeon/qqbot/Welcome.kt
@@ -2,15 +2,20 @@ package pigeon.qqbot
 
 import kotlinx.coroutines.delay
 import net.mamoe.mirai.Bot
+import net.mamoe.mirai.event.events.MemberJoinEvent
 import net.mamoe.mirai.event.events.NewFriendRequestEvent
 import net.mamoe.mirai.event.subscribeAlways
 
 fun Bot.welcome() {
-    this.subscribeAlways<NewFriendRequestEvent> { event ->
+    this.subscribeAlways<NewFriendRequestEvent> {
         if (this.fromGroupId == 596870824L) {//好友请求来自组群
-            event.accept()
+            it.accept()
             delay(3000L)
-            bot.getFriend(this.fromId).sendMessage("test")
+            bot.getFriend(it.fromId).sendMessage("test")
         }
+    }
+
+    this.subscribeAlways<MemberJoinEvent> {
+        bot.getGroup(it.group.id).sendMessage("欢迎新人！群地位-1")
     }
 }
---
+++ src/main/setuserver.py
@@ -0,0 +1,26 @@
+from pixivpy3 import PixivAPI 
+import rpyc
+import random
+import sys
+
+_REQUESTS_KWARGS = {
+     #'proxies': {
+     #    'https': 'http://127.0.0.1:1080',
+     #},
+     #'verify': True,
+}
+
+class SetuService(rpyc.Service):
+    def __init__(self,username,password):
+        super().__init__()
+        self._username=username
+        self._password=password
+    def on_connect(self, conn):
+        self._api=PixivAPI(**_REQUESTS_KWARGS)
+        self._api.login(self._username,self._password)
+    def exposed_search(self,keyword):
+         self._illusts=self._api.search_works(keyword,types=["illustration"],per_page=1000).response
+         self._illusts.sort(key=lambda illust:-illust.stats.views_count)
+         return self._illusts[0].image_urls.large.replace("i.pximg.net","i.pixiv.cat")
+
+rpyc.utils.server.ThreadedServer(SetuService(sys.argv[1],sys.argv[2]),port=11451).start()
