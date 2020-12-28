2177fc4c0b012fce52aec7f3b96b5e18d393d40c
veraku <a146665154@163.com>
2020-12-28 20:30:29 +0800

    色图bug修复

--- src/main/setuserver.py
+++ src/main/setuserver.py
@@ -11,34 +11,28 @@ _REQUESTS_KWARGS = {
      #'verify': True,
 }
 
-
-
 class SetuService(rpyc.Service):
     def __init__(self,username,password):
         super().__init__()
         self._username=username
         self._password=password
-        self._fglfile= open('src/main/resources/fgl.txt','r')
+        _fglfile= open('src/main/resources/fgl.txt','r')
         self._fgl = self._fglfile.readlines()
-        self._fglfile.close()
-    def on_connect(self, conn):
+        _fglfile.close()
         self._api=PixivAPI(**_REQUESTS_KWARGS)
         self._api.login(self._username,self._password)
     def exposed_search(self,keyword,mode):
-        #self._illusts=self._api.search_works(keyword,types=["illustration"],per_page=1000,mode=mode).response
-        #self._illusts.sort(key=lambda illust:-illust.stats.views_count)
-        #return self._illusts[random.randint(0,5)].image_urls.large.replace("i.pximg.net","i.pixiv.cat")
-        self._illusts=self._api.search_works(keyword,types=["illustration"],per_page=500,mode=mode,sort="popular").response
-        fglFile = open('src/main/resources/fgl.txt', 'r')
-        for i in self._illusts:
+        _illusts=self._api.search_works(keyword,types=["illustration"],per_page=500,mode=mode,sort="popular").response
+        for i in _illusts:
             if str(i.id) not in self._fgl:
                 print(self._fgl)
-                f = open('src/main/resources/fgl.txt', 'w')
+                f = open('src/main/resources/fgl.txt', 'w+')
                 self._fgl.append(str(i.id))
                 f.write(str(i.id) + '\n')
                 f.close()
                 return i.image_urls.large.replace("i.pximg.net","i.pixiv.cat")
                 break
+        return ""
 
 
 rpyc.utils.server.ThreadedServer(SetuService(sys.argv[1],sys.argv[2]),port=11451).start()
