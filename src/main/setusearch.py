84898b3c0306c525da490ae11b9ecb83d0e80a26
willbe <aotuman233@gmail.com>
2021-01-23 01:58:11 +0800

    formatted & per_page 1000->500

--- src/main/setusearch.py
+++ src/main/setusearch.py
@@ -2,32 +2,35 @@ import sys
 from pixivpy3 import PixivAPI
 
 _REQUESTS_KWARGS = {
-     'proxies': {
-         'https': 'http://127.0.0.1:8118',
-     },
-     'verify': True,
+    'proxies': {
+        'https': 'http://127.0.0.1:8118',
+    },
+    'verify': True,
 }
 
+
 class SetuService():
-    def __init__(self,username,password):
-        self._username=username
-        self._password=password
-        _fglfile= open('src/main/resources/fgl.txt','r')
+    def __init__(self, username, password):
+        self._username = username
+        self._password = password
+        _fglfile = open('src/main/resources/fgl.txt', 'r')
         self._fgl = []
         for line in _fglfile.readlines():
             self._fgl.append(line.strip())
         _fglfile.close()
-        self._api=PixivAPI(**_REQUESTS_KWARGS)
-        self._api.login(self._username,self._password)
-    def search(self,keyword,mode):
-        _res=self._api.search_works(keyword,types=["illustration"],per_page=1000,mode=mode,sort="popular")
-        _illusts=_res.response
+        self._api = PixivAPI(**_REQUESTS_KWARGS)
+        self._api.login(self._username, self._password)
+
+    def search(self, keyword, mode):
+        _res = self._api.search_works(keyword, types=["illustration"], per_page=500, mode=mode, sort="popular")
+        _illusts = _res.response
         for i in _illusts:
             if str(i.id) not in self._fgl:
                 f = open('src/main/resources/fgl.txt', 'a')
                 self._fgl.append(str(i.id))
                 f.write(str(i.id) + '\n')
                 f.close()
-                return i.image_urls.large.replace("i.pximg.net","i.pixiv.cat")+" "+str(i.id)
+                return i.image_urls.large.replace("i.pximg.net", "i.pixiv.cat") + " " + str(i.id)
 
-print(SetuService(sys.argv[1],sys.argv[2]).search(sys.argv[3],sys.argv[4]))
+
+print(SetuService(sys.argv[1], sys.argv[2]).search(sys.argv[3], sys.argv[4]))
