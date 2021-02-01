845142158303c03cfc29ea9fb7bef4ce152e7009
willbe <aotuman233@gmail.com>
2021-02-01 19:23:04 +0800

    fixed

--- src/main/kotlin/pigeon/qqbot/Help.kt
+++ src/main/kotlin/pigeon/qqbot/Help.kt
@@ -2,6 +2,8 @@ package pigeon.qqbot
 
 import net.mamoe.mirai.Bot
 import net.mamoe.mirai.event.subscribeMessages
+import net.mamoe.mirai.message.sendAsImageTo
+import java.io.File
 
 fun Bot.help(){
     this.subscribeMessages {
