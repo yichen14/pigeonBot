852258bb9f4aadaaac329e5bb82199caa451fa10
Ivanfangsc <31620942+Ivanfangsc@users.noreply.github.com>
2021-05-07 20:45:23 +0800

    修色图

--- src/main/setusearch.py
+++ src/main/setusearch.py
@@ -3,44 +3,35 @@ from pixivpy3 import PixivAPI
 
 _REQUESTS_KWARGS = {
     'proxies': {
-        'https': 'http://127.0.0.1:8118',
+        'https': 'http://127.0.0.1:1080',
     },
     'verify': True,
 }
-
-
-class SetuService():
-    def __init__(self, username, password):
-        self._username = username
-        self._password = password
-        _fglfile = open('src/main/resources/fgl.txt', 'r')
-        self._fgl = []
-        for line in _fglfile.readlines():
-            self._fgl.append(line.strip())
-        _fglfile.close()
-        self._api = PixivAPI(**_REQUESTS_KWARGS)
-        self._api.login(self._username, self._password)
-
-    def search(self, keyword, mode):
-        _illusts = None
-        if keyword == "ranking":
-            _illusts = self._api.ranking_all().response[0].works
-            for i in _illusts:
-                if str(i.work.id) not in self._fgl:
-                    f = open('src/main/resources/fgl.txt', 'a')
-                    self._fgl.append(str(i.work.id))
-                    f.write(str(i.work.id) + '\n')
-                    f.close()
-                    return i.work.image_urls.large.replace("i.pximg.net", "i.pixiv.cat") + " " + str(i.work.id)
-        else:
-            _illusts = self._api.search_works(keyword, types=["illustration"], per_page=500, mode=mode, sort="popular").response
-            for i in _illusts:
-                if str(i.id) not in self._fgl:
-                    f = open('src/main/resources/fgl.txt', 'a')
-                    self._fgl.append(str(i.id))
-                    f.write(str(i.id) + '\n')
-                    f.close()
-                    return i.image_urls.large.replace("i.pximg.net", "i.pixiv.cat") + " " + str(i.id)
-
-
-print(SetuService(sys.argv[1], sys.argv[2]).search(sys.argv[3], sys.argv[4]))
+fglfile = open('src/main/resources/fgl.txt', 'r')
+fgl = []
+for line in fglfile.readlines():
+    fgl.append(line.strip())
+fglfile.close()
+api = PixivAPI(**_REQUESTS_KWARGS)
+api.auth(refresh_token=sys.argv[1])
+illusts = None
+if sys.argv[2] == "ranking":
+    illusts = api.ranking_all().response[0].works
+    for i in illusts:
+        if str(i.work.id) not in fgl:
+            f = open('src/main/resources/fgl.txt', 'a')
+            fgl.append(str(i.work.id))
+            f.write(str(i.work.id) + '\n')
+            f.close()
+            print(i.work.image_urls.large.replace("i.pximg.net", "i.pixiv.cat") + " " + str(i.work.id))
+            break
+else:
+    illusts = api.search_works(sys.argv[2], types=["illustration"], per_page=500, mode=sys.argv[3], sort="popular").response
+    for i in illusts:
+        if str(i.id) not in fgl:
+            f = open('src/main/resources/fgl.txt', 'a')
+            fgl.append(str(i.id))
+            f.write(str(i.id) + '\n')
+            f.close()
+            print(i.image_urls.large.replace("i.pximg.net", "i.pixiv.cat") + " " + str(i.id))
+            break
